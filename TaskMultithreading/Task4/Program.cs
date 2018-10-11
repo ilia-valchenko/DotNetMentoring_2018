114d685d2baf32e91ac833de4aba25e56ccc3eb3
Ilya Valchanka <Ilya_Valchanka@epam.com>
2018-10-11 18:51:12 +0300

    Added Task 4.

---
+++ TaskMultithreading/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The Main thread. ThreadId = {Thread.CurrentThread.ManagedThreadId}");
+
+            int counter = 10;
+
+            DecrementCounter(counter);
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey();
+        }
+
+        public static void DecrementCounter(object state)
+        {
+            int counter = (int)state;
+
+            if (counter >= 0)
+            {
+                Console.WriteLine($"Counter = {counter}. ThreadId = {Thread.CurrentThread.ManagedThreadId}");
+                counter--;
+                var t = new Thread(DecrementCounter);
+                t.Start(counter);
+                t.Join();
+            }
+            else
+            {
+                Console.WriteLine("Counter is less then zero or equal to it.");
+            }
+        }
+    }
+}
