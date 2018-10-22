d0cd0554fbdb3689fd07df91cfba536b10278772
Ilya Valchanka <Ilya_Valchanka@epam.com>
2018-10-22 16:12:18 +0300

    Finished Multithreading module task.

--- MultithreadingModuleTask/NamedPipesClientServer/ClientConsoleApplication/Program.cs
+++ MultithreadingModuleTask/NamedPipesClientServer/ClientConsoleApplication/Program.cs
@@ -5,44 +5,49 @@ namespace ClientConsoleApplication
 {
     class Program
     {
-        private const string PipeName = "TestPipeName";
-        private const string StopCommand = "stop";
+        private const string ServerName = ".";
         private const int NumberOfSendingMessages = 5;
 
-        static void Main(string[] args)
+        static void Main()
         {
             var clients = CreateClients();
 
             for(int i = 0; i < clients.Count; i++)
             {
-                Console.WriteLine("[CLIENT]: Client started.");
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} started.");
 
-                clients[i].ConnectToServer(PipeName);
+                clients[i].ConnectToServer(ServerName);
 
-                Console.WriteLine("[CLIENT] Connected to the server.");
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} connected to the server.");
 
-                // First client don't want to read the history of messages from
-                // the server. It still has empty.
-                if (i != 0)
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} is getting history of messages from the server...");
+
+                var historyOfMessages = clients[i].GetHistoryOfMessagesFromServer();
+
+                for (int j = 0; j < historyOfMessages.Count - 1; j++)
                 {
-                    var historyOfMessages = clients[i].GetHistoryOfMessagesFromServer();
-
-                    Console.WriteLine("[CLIENT]: Get history of messages from the server.");
-
-                    for (int j = 0; j < historyOfMessages.Count - j; i++)
-                    {
-                        Console.WriteLine($"[CLIENT]: #{j + 1} - {historyOfMessages[j]}");
-                    }
+                    Console.WriteLine($"[CLIENT]: #{j + 1} - {historyOfMessages[j]}");
                 }
 
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} successfully received the history of messages from the server.");
+
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} is sending messages to the server...");
+
                 clients[i].SendMessagesToServer(NumberOfSendingMessages);
-                clients[i].DisconnectFromServer();
+
+                Console.WriteLine($"[CLIENT]: Client #{i + 1} successfully send {NumberOfSendingMessages} messages to the server.");
             }
 
             Console.WriteLine("\n\nTap to close the window...");
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Creates initial set of clients.
+        /// </summary>
+        /// <returns>
+        /// Returns the collection of initial clients.
+        /// </returns>
         private static List<Client> CreateClients()
         {
             return new List<Client>
