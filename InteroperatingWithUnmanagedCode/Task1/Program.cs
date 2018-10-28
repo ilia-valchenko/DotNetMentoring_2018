4dbf608853ed8437c0e06660281436fa62dce26a
Ilya Valchanka <ilia.valchenko.epam@gmail.com>
2018-10-28 23:20:05 +0300

    Created empty VS solution for Interoperating with unmanaged code module tasks.

---
+++ InteroperatingWithUnmanagedCode/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey();
+        }
+    }
+}
