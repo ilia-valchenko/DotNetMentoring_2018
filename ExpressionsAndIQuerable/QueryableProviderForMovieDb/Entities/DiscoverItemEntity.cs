720173902a101eee256aedcdc50a9bda2ad590c4
Ilya Valchanka <ilia.valchenko.epam@gmail.com>
2018-11-14 00:14:00 +0300

    Added project which uses The Movie Database API.

---
+++ ExpressionsAndIQuerable/QueryableProviderForMovieDb/Entities/DiscoverItemEntity.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace QueryableProviderForMovieDb.Entities
+{
+    public class DiscoverItemEntity
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("release_date")]
+        public string ReleaseDate { get; set; }
+
+        [JsonProperty("vote_average")]
+        public double VoteAverage { get; set; }
+    }
+}
---
+++ ExpressionsAndIQuerable/QueryableProviderForMovieDb/Entities/DiscoveryEntity.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace QueryableProviderForMovieDb.Entities
+{
+    public class DiscoveryEntity
+    {
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+        [JsonProperty("results")]
+        public List<DiscoverItemEntity> Results { get; set; }
+
+        [JsonProperty("total_results")]
+        public int TotalResults { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+    }
+}
